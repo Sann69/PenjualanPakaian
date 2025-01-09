7e2a81c5a1f9b5eaefb3004349864d2331a4c724
Jati Sanjaya <jsan3191@gmail.com>
2025-01-09 10:06:47 +0800

    CRUD Menu Produk

--- app/src/main/java/com/example/penjualanpakaian/MenuProdukActivity.java
+++ app/src/main/java/com/example/penjualanpakaian/MenuProdukActivity.java
@@ -1,24 +1,57 @@
 package com.example.penjualanpakaian;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MenuProdukActivity extends AppCompatActivity {
+    GridView gridProduk;
+    Button btnTambahProduk, btnKembali;
+    DatabaseHelperProduk databaseHelperProduk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu_produk);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        databaseHelperProduk = new DatabaseHelperProduk(this);
+
+        gridProduk = findViewById(R.id.gridViewData);
+        btnTambahProduk = findViewById(R.id.buttonTambahProduk);
+        btnKembali = findViewById(R.id.buttonKembali);
+
+        btnTambahProduk.setOnClickListener(v -> {
+            Intent intent = new Intent(MenuProdukActivity.this, ManageDataProduk.class);
+            startActivity(intent);
+        });
+
+        btnKembali.setOnClickListener(v -> {
+            Intent intent = new Intent(MenuProdukActivity.this, MenuUtamaActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        loadProdukData();
+    }
+
+    private void loadProdukData() {
+        ArrayList<HashMap<String, String>> produkList = databaseHelperProduk.getAllProduk();
+        ProdukAdapter adapter = new ProdukAdapter(this, produkList);
+        gridProduk.setAdapter(adapter);
+
+        gridProduk.setOnItemClickListener((parent, view, position, id) -> {
+            HashMap<String, String> selectedProduk = produkList.get(position);
+            Intent intent = new Intent(MenuProdukActivity.this, ManageDataProduk.class);
+            intent.putExtra("id", selectedProduk.get("id"));
+            intent.putExtra("mode", "update");
+            startActivity(intent);
         });
     }
 }
