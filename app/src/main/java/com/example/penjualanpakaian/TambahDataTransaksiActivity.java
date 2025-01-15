a7da6ca310505fe9346c691f0820faa4fa85622e
Angelina-a11y <putusri2334@gmail.com>
2025-01-15 20:37:19 +0800

    add spinner in transaksi

--- app/src/main/java/com/example/penjualanpakaian/TambahDataTransaksiActivity.java
+++ app/src/main/java/com/example/penjualanpakaian/TambahDataTransaksiActivity.java
@@ -2,31 +2,68 @@ package com.example.penjualanpakaian;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class TambahDataTransaksiActivity extends AppCompatActivity {
 
+    private Spinner selectProduct;
     private EditText etProduk, etHarga, etQty;
     private TextView tvTotal;
     private Button btnSimpan;
     private DatabaseHelperTransaksi databaseHelper;
+
+    private DatabaseHelperProduk databaseHelperProduk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_data_transaksi);
 
 
-        etProduk = findViewById(R.id.editTextText);
+//        etProduk = findViewById(R.id.editTextText);
+        selectProduct = findViewById(R.id.selectProduct);
         etHarga = findViewById(R.id.editTextText2);
         etQty = findViewById(R.id.editTextText3);
         tvTotal = findViewById(R.id.textTotal);
         btnSimpan = findViewById(R.id.buttonSimpan);
 
         databaseHelper = new DatabaseHelperTransaksi(this);
+        databaseHelperProduk = new DatabaseHelperProduk(this);
+
+        ArrayList<HashMap<String, String>> produkList = databaseHelperProduk.getAllProduk();
+
+        List<String> namaProdukList = new ArrayList<>();
+        for (HashMap<String, String> produk : produkList){
+            namaProdukList.add(produk.get("nama"));
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, namaProdukList);
+
+        selectProduct.setAdapter(adapter);
+
+//        set onItemSelectedListener utk spinner
+        selectProduct.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String harga = produkList.get(position).get("harga");
+
+                etHarga.setText(harga);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
         etHarga.addTextChangedListener(new android.text.TextWatcher() {
